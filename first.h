8cfb259aaf37431e63c37c7145e2e16254ee78cc
Pu Chen <puchen8295@gmail.com>
2017-10-22 18:19:31 -0400

    Delete first.h

--- first.h
+++
@@ -1,5 +0,0 @@
-// Header file for assignment 4 
-
-int getSetIndex(char binaryPtr[], int numberOfTagBits, int numberOfSetIndexBits);
-int getSetIndexForTypeB(char binaryPtr[], int numberOfSetIndexBits);
-char* hexToBinaryHelper(char hexChar);
