666a5ae62ee6cf934c6a366461acd2c6a9e969fa
PuChen7 <puchen8295@gmail.com>
2017-10-22 18:18:12 -0400

    upload source files

---
+++ first.h
@@ -0,0 +1,5 @@
+// Header file for assignment 4 
+
+int getSetIndex(char binaryPtr[], int numberOfTagBits, int numberOfSetIndexBits);
+int getSetIndexForTypeB(char binaryPtr[], int numberOfSetIndexBits);
+char* hexToBinaryHelper(char hexChar);
